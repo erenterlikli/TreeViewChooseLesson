08efaa0df8a6568cb14fc098ffcb3511619b5380
eren.terlikli <44238447+erenterlikli@users.noreply.github.com>
2020-10-27 18:54:24 +0300

    Add files via upload

---
+++ TreeView/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace TreeView
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(12, 62);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(581, 212);
+            this.treeView1.TabIndex = 0;
+            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(171, 44);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Dersleri Göster";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(189, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 31);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Ders:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(13, 307);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(580, 95);
+            this.listBox1.TabIndex = 3;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.PeachPuff;
+            this.ClientSize = new System.Drawing.Size(673, 450);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.treeView1);
+            this.Name = "Form1";
+            this.Text = "Ders Seçimi";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ TreeView/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TreeView
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            label1.Text = e.Node.ToString();
+            listBox1.Items.Add(e.Node.ToString());
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            TreeNode ekle = new TreeNode("Dersler");
+            treeView1.Nodes.Add(ekle);
+
+
+            TreeNode cocuk1 = new TreeNode("Matematik");
+            TreeNode cocuk2 = new TreeNode("Geometri");
+            TreeNode cocuk3 = new TreeNode("Fizik");
+            TreeNode cocuk4 = new TreeNode("Kimya");
+            TreeNode cocuk5 = new TreeNode("Biyoloji");
+
+            TreeNode[] array = new TreeNode[] { cocuk1, cocuk2, cocuk3, cocuk4, cocuk5 };
+
+            ekle = new TreeNode("Sayısal Dersler", array);
+            treeView1.Nodes.Add(ekle);
+
+
+            TreeNode cocuk6 = new TreeNode("Türk Dili ve Edebiyatı");
+            TreeNode cocuk7 = new TreeNode("Dil ve Anlatım");
+            TreeNode cocuk8 = new TreeNode("Tarih");
+            TreeNode cocuk9 = new TreeNode("Coğrafya");
+            TreeNode cocuk10 = new TreeNode("Felsefe Grubu");
+
+            TreeNode[] array2 = new TreeNode[] { cocuk6, cocuk7, cocuk8, cocuk9, cocuk10 };
+
+            ekle = new TreeNode("Sözel Dersler", array2);
+            treeView1.Nodes.Add(ekle);
+
+            TreeNode cocuk11 = new TreeNode("İngilizce");
+            TreeNode cocuk12 = new TreeNode("Almanca");
+            TreeNode cocuk13 = new TreeNode("Fransızca");
+            TreeNode cocuk14 = new TreeNode("İspanyolca");
+            TreeNode cocuk15 = new TreeNode("İtalyanca");
+
+            TreeNode[] array3 = new TreeNode[] { cocuk11, cocuk12, cocuk13, cocuk14, cocuk15 };
+
+            ekle = new TreeNode("Dil Dersleri", array3);
+            treeView1.Nodes.Add(ekle);
+
+
+            TreeNode cocuk16 = new TreeNode("Beden Eğitimi ve Spor");
+            TreeNode cocuk17 = new TreeNode("Görsel Sanatlar");
+            TreeNode cocuk18 = new TreeNode("Müzik");
+            TreeNode cocuk19 = new TreeNode("Drama");
+            TreeNode cocuk20 = new TreeNode("El Sanatları");
+
+            TreeNode[] array4 = new TreeNode[] { cocuk16, cocuk17, cocuk18, cocuk19, cocuk20 };
+
+            ekle = new TreeNode("Hobi Dersleri", array4);
+            treeView1.Nodes.Add(ekle);
+
+            TreeNode cocuk21 = new TreeNode("Astronomi ve Uzay Bilimleri");
+            TreeNode cocuk22 = new TreeNode("Bilgisayar ve Yazılım");
+            TreeNode cocuk23 = new TreeNode("Sanat Tarihi");
+            TreeNode cocuk24 = new TreeNode("Sağlık Bilgisi");
+            TreeNode cocuk25 = new TreeNode("Yaratıcı Yazarlık");
+
+            TreeNode[] array5 = new TreeNode[] { cocuk21, cocuk22, cocuk23, cocuk24, cocuk25 };
+
+            ekle = new TreeNode("Genel Kültür Dersleri", array5);
+            treeView1.Nodes.Add(ekle);
+
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+           
+        }
+    }
+}
